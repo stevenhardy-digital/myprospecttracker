3a142825e8158f640fce7eb0103b39bf891e3c16
Steven Hardy <steven@stevenhardy.digital>
2025-05-22 13:12:56 +0100

    landing page

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,78 +1,47 @@
 <!-- main-footer -->
-<footer class="main-footer">
-    <div class="image-layer" style="background-image: url(images/icons/footer-bg-3.png);"></div>
+<footer class="main-footer bg-light py-5">
     <div class="container">
-        <div class="footer-top">
-            <div class="widget-section">
-                <div class="row">
-                    <div class="col-lg-4 col-md-6 col-sm-12 footer-column">
-                        <div class="about-widget footer-widget">
-                            <figure class="footer-logo"><a href="index.html"><img src="images/footer-logo.png" alt=""></a></figure>
-                            <div class="text">Lorem ipsum dolor sit consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim lorem sed do eiusmod.</div>
-                            <ul class="social-links">
-                                <li><h6>Follow Us :</h6></li>
-                                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                                <li><a href="#"><i class="fab fa-skype"></i></a></li>
-                                <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 col-sm-12 footer-column">
-                        <div class="links-widget footer-widget">
-                            <h4 class="widget-title">Support</h4>
-                            <div class="widget-content">
-                                <ul class="list clearfix">
-                                    <li><a href="#">Contact Us</a></li>
-                                    <li><a href="#">Submit a Ticket</a></li>
-                                    <li><a href="#">Visit Knowledge Base</a></li>
-                                    <li><a href="#">Support System</a></li>
-                                    <li><a href="#">Refund Policy</a></li>
-                                    <li><a href="#">Professional Services</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-2 col-md-6 col-sm-12 footer-column">
-                        <div class="links-widget footer-widget">
-                            <h4 class="widget-title">Links</h4>
-                            <div class="widget-content">
-                                <ul class="list clearfix">
-                                    <li><a href="#">Home</a></li>
-                                    <li><a href="#">Services</a></li>
-                                    <li><a href="#">Price Plan</a></li>
-                                    <li><a href="#">Testimonials</a></li>
-                                    <li><a href="#">News</a></li>
-                                    <li><a href="#">Contact</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 col-sm-12 footer-column">
-                        <div class="contact-widget footer-widget">
-                            <h4 class="widget-title">Contact Info</h4>
-                            <div class="widget-content">
-                                <ul class="list clearfix">
-                                    <li><i class="fas fa-map-marker-alt"></i>25 Bedford St.<br />New York City, N.Y.</li>
-                                    <li>
-                                        <i class="fas fa-phone-volume"></i>
-                                        <a href="tel:+0665184575181">(+066) 518 - 457 - 5181</a><br />
-                                        <a href="tel:+0665184575182">(+066) 518 - 457 - 5182</a>
-                                    </li>
-                                    <li>
-                                        <i class="fas fa-envelope"></i>
-                                        <a href="mailto:info@example.com">info@example.com</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+        <div class="row align-items-center text-center text-md-start">
+
+            <!-- Logo & About -->
+            <div class="col-md-4 mb-4 mb-md-0">
+                <a href="/" class="d-inline-block mb-2">
+                    <img src="images/footer-logo.png" alt="My Prospect Tracker" style="max-width: 160px;">
+                </a>
+                <p class="text-muted small">
+                    A smarter way to manage your follow-ups. Built for focus, flow, and freedom.
+                </p>
             </div>
+
+            <!-- Quick Links -->
+            <div class="col-md-4 mb-4 mb-md-0">
+                <h6 class="fw-bold text-dark mb-3">Quick Links</h6>
+                <ul class="list-unstyled text-muted small">
+                    <li><a href="/" class="text-muted">Home</a></li>
+                    <li><a href="#features" class="text-muted">Features</a></li>
+                    <li><a href="#pricing" class="text-muted">Pricing</a></li>
+                    <li><a href="#contact" class="text-muted">Contact</a></li>
+                </ul>
+            </div>
+
+            <!-- Social Media -->
+{{--            <div class="col-md-4">--}}
+{{--                <h6 class="fw-bold text-dark mb-3">Follow Us</h6>--}}
+{{--                <div class="d-flex justify-content-center justify-content-md-start gap-3">--}}
+{{--                    <a href="#" class="text-primary fs-5"><i class="fab fa-facebook-f"></i></a>--}}
+{{--                    <a href="#" class="text-primary fs-5"><i class="fab fa-twitter"></i></a>--}}
+{{--                    <a href="#" class="text-primary fs-5"><i class="fab fa-instagram"></i></a>--}}
+{{--                    <a href="#" class="text-primary fs-5"><i class="fab fa-linkedin-in"></i></a>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+
         </div>
-        <div class="footer-bottom">
-            <div class="copyright">&copy; 2020 <a href="#">appway</a>. All rights reserved</div>
+
+        <hr class="my-4" />
+
+        <!-- Copyright -->
+        <div class="text-center text-muted small">
+            &copy; {{ date('Y') }} My Prospect Tracker. All rights reserved.
         </div>
     </div>
 </footer>
-<!-- main-footer end -->
