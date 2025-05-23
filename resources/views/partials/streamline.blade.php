08de8e45c70087f064c285cd9e1e563413851f13
Steven Hardy <steven@stevenhardy.digital>
2025-05-23 15:24:52 +0100

    landing page

--- resources/views/partials/streamline.blade.php
+++ resources/views/partials/streamline.blade.php
@@ -26,7 +26,7 @@
             <div class="col-lg-6 col-md-12 col-sm-12 image-column">
                 <div id="image_block_33">
                     <div class="image-box wow slideInRight" data-wow-delay="300ms" data-wow-duration="1500ms">
-                        <figure class="image clearfix js-tilt"><img src="images/resource/illustration-19.png" alt=""></figure>
+                        <figure class="image clearfix js-tilt"><img src="images/resource/mpt-5.png" alt=""></figure>
                     </div>
                 </div>
             </div>
