82e57b55a3fbf4b42fc1213abd981ee0923d5b13
Steven Hardy <steven@stevenhardy.digital>
2025-05-25 12:12:33 +0100

    landing page

--- resources/views/waitlist.blade.php
+++ resources/views/waitlist.blade.php
@@ -3,13 +3,11 @@
 @section('title', 'My Prospect Tracker | Join the Mailing List')
 
 @section('content')
-    <div class="min-h-screen flex items-center justify-center px-4">
-        <div class="w-full max-w-lg bg-white rounded-xl shadow p-8 text-center">
-
-            <h1 class="text-3xl font-bold text-primary mb-4">Something Big is Coming</h1>
-            <p class="text-gray-700 text-lg leading-relaxed">
-                At the moment we are planning and launching something big.
-                <br><br>
+    <div class="waitlist-container">
+        <div class="waitlist-box">
+            <h1>Something Big is Coming</h1>
+            <p>
+                At the moment we are planning and launching something big.<br><br>
                 Why don't you register to join our mailing list?
             </p>
         </div>
