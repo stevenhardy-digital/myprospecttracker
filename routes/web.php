2f8b5ea6d6ef0fe8e9b645333f2256f8e1f851c7
Steven Hardy <steven@stevenhardy.digital>
2025-05-28 21:58:15 +0100

    admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,9 @@ Route::middleware(['auth', \App\Http\Middleware\ProOnly::class])->group(function
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    //Prospects
+    Route::post('/prospects', [ProspectController::class, 'store'])->name('prospects.store');
 });
 
 require __DIR__.'/auth.php';
