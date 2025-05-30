3fd8d65b40dfaab6230a4e3b9423cdfee7cd2fdc
Steven Hardy <steven@stevenhardy.digital>
2025-05-30 12:53:30 +0100

    admin dashboard

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -16,6 +15,4 @@ export default {
             },
         },
     },
-
-    plugins: [forms],
 };
