c47a2c3776258a9b53f9cd4d711889494d6cb542
Steven Hardy <steven@stevenhardy.digital>
2025-05-30 11:52:22 +0100

    admin dashboard

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,18 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/js/app.js',
-                'resources/sass/app.scss',
-            ],
-            refresh: true,
-        }),
-    ],
+    plugins: [laravel(['resources/sass/app.scss', 'resources/js/app.js'])],
+    build: {
+        assetsInlineLimit: 0,
+        rollupOptions: {
+            output: {
+                assetFileNames: 'assets/[name].[hash][extname]',
+            },
+        },
+    },
+    resolve: {
+        alias: {
+            '@': '/resources',
+        },
+    },
 });
