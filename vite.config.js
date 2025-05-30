7103301d2aad893b6e4e787795c333131f20cd99
Steven Hardy <steven@stevenhardy.digital>
2025-05-30 12:35:10 +0100

    admin dashboard

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,8 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-    plugins: [
-        laravel([
-            'resources/sass/app.scss',
-            'resources/sass/admin.scss',
-            'resources/js/app.js',
-        ]),
-        viteStaticCopy({
-            targets: [
-                {
-                    src: 'node_modules/bootstrap-icons/font/*.woff2',
-                    dest: 'assets/fonts'
-                },
-                {
-                    src: 'node_modules/bootstrap-icons/font/*.woff',
-                    dest: 'assets/fonts'
-                },
-                {
-                    src: 'node_modules/bootstrap-icons/font/bootstrap-icons.min.css',
-                    dest: 'assets/css'
-                }
-            ]
-        })
-    ],
-    assetsInclude: ['**/*.woff', '**/*.woff2'],
+    plugins: [laravel(['resources/sass/app.scss', 'resources/sass/admin.scss', 'resources/js/app.js'])],
     build: {
         assetsInlineLimit: 0,
         rollupOptions: {
