02a4cc21ac91ffdefa29d40d680cbe7eb861acd9
Steven Hardy <steven@stevenhardy.digital>
2025-05-30 12:10:39 +0100

    admin dashboard

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [laravel(['resources/sass/app.scss', 'resources/sass/admin.scss', 'resources/js/app.js'])],
+    assetsInclude: ['**/*.woff', '**/*.woff2'],
     build: {
         assetsInlineLimit: 0,
         rollupOptions: {
